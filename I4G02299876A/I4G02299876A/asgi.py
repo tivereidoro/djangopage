f3dd91ca70a2da62cdba56d2b5ef1431c7aa1ae0
Tivere IDORO <tivereidoro@yahoo.com>
2024-02-02 23:49:34 +0100

    Update asgi.py

--- I4G02299876A/I4G02299876A/asgi.py
+++ I4G02299876A/I4G02299876A/asgi.py
@@ -6,6 +6,8 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 """
+#!/usr/bin/python3
+
 
 import os
 
